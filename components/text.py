1eddeb3952cd3810c4e2f3bf727553689131b878
singhprincejeet <singhprincejeet@gmail.com>
2019-10-26 20:32:57 -0400

    First Commit

---
+++ components/text.py
@@ -0,0 +1,20 @@
+class Text:
+    def __init__(self, value, text_style):
+        self.value = value
+        self.text_style = text_style
+
+
+    def get_font(self):
+        return self.text_style.get_font()
+
+
+    def get_value(self):
+        return self.value
+
+
+    def get_align(self):
+        return self.text_style.get_align()
+
+
+    def get_color(self):
+        return self.text_style.get_color()
